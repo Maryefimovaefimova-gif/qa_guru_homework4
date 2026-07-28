d6bb89420c1023049b100024b13f6d237df26278
QA <aishams83@mail.ru>
2026-07-28 22:34:32 +0300

    reformat_code

--- simple_test_student_registration_form.py
+++ simple_test_student_registration_form.py
@@ -1,11 +1,12 @@
-import time
 import os
+import time
 import unittest
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 # python -m unittest simple_test_student_registration_form.py
